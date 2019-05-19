4fbbe6864cfb63e16d06acfc1b3f652d700edac6
electrifymylife <vikhrovagp@gmail.com>
2019-05-19 22:21:27 +0300

    модальная форма + js, шапка + js, интерактивная карта, правки по тз

---
+++ source/js/modal-catalog.js
@@ -0,0 +1,18 @@
+var linkCatalog = document.querySelectorAll(".products__item-buy");
+var popup = document.querySelector(".modal");
+
+linkCatalog.forEach(function (item) {
+  item.addEventListener('click', function (evt) {
+    evt.preventDefault;
+    popup.classList.add("modal-show");
+  });
+   });
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (popup.classList.contains("modal-show")) {
+      evt.preventDefault();
+      popup.classList.remove("modal-show");
+    }
+  }
+});
