23e2753556f5cac4fd518507fb641d16e934ea7c
electrifymylife <vikhrovagp@gmail.com>
2019-05-29 17:35:52 +0300

    правки по критериям

--- source/js/modal-catalog.js
+++ source/js/modal-catalog.js
@@ -4,15 +4,15 @@ var popup = document.querySelector(".modal");
 linkCatalog.forEach(function (item) {
   item.addEventListener('click', function (evt) {
     evt.preventDefault;
-    popup.classList.add("modal-show");
+    popup.classList.add("modal--show");
   });
 });
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
-    if (popup.classList.contains("modal-show")) {
+    if (popup.classList.contains("modal--show")) {
       evt.preventDefault();
-      popup.classList.remove("modal-show");
+      popup.classList.remove("modal--show");
     }
   }
 });
--- source/js/modal.js
+++ source/js/modal.js
@@ -3,14 +3,14 @@ var popup = document.querySelector(".modal");
 
 linkIndex.addEventListener("click", function (evt) {
   evt.preventDefault;
-  popup.classList.add("modal-show");
+  popup.classList.add("modal--show");
 });
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
-    if (popup.classList.contains("modal-show")) {
+    if (popup.classList.contains("modal--show")) {
       evt.preventDefault();
-      popup.classList.remove("modal-show");
+      popup.classList.remove("modal--show");
     }
   }
 });
